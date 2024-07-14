820d643d9482ce0376fa8e7c4324bcf3750421e9
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-07-14 14:41:29 +0200

    Intial source
    - added provisioning
    - added docker compose config with db
    - added running scripts

---
+++ provisioning/arch-provisioning.sh
@@ -0,0 +1,30 @@
+#!/bin/zsh
+
+# Update packages
+sudo pacman -Syu
+
+# install docker if needed
+sudo pacman -S --needed docker
+sudo pacman -S --needed docker-compose
+
+# Check if the user is in the docker group
+if id -nG "$USER" | grep -qw "docker"; then
+    echo "User $USER is already in the docker group."
+else
+    echo "User $USER is not in the docker group. Adding now..."
+    sudo usermod -aG docker "$USER"
+
+    # Check if the command succeeded
+    if [ $? -eq 0 ]; then
+        echo "User $USER was successfully added to the docker group."
+    else
+        echo "Failed to add user $USER to the docker group."
+        exit 1
+    fi
+fi
+
+# Config docker deamon on demand
+sudo systemctl disable docker.service
+sudo systemctl stop docker.service
+sudo systemctl enable docker.socket
+sudo systemctl start docker.socket
