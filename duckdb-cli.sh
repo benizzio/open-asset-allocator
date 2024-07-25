daa9f50e61c24ff48a44b27e2eee6310f84c4e1a
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-07-25 22:14:25 +0200

    improving duckdb scripts

--- duckdb-cli.sh
+++ duckdb-cli.sh
@@ -1,10 +1,34 @@
 #!/bin/zsh
 
-cd src/main/docker/duckdb
+script_dir=$(dirname "$0")
+project_root=$(realpath "$script_dir")
+
+target_relative_path=target/duckdb-input
+std_input_file=$project_root/$target_relative_path/input.sql
+
+if [ ! -d $project_root/$target_relative_path ]; then
+    mkdir $project_root/$target_relative_path
+elseif [ -f $std_input_file ]
+    rm $std_input_file
+fi
+
 
 if [ -n "$1" ]; then
-    echo "First parameter is: $1"
-    docker compose -f docker-compose-duckdb.yml run --rm --env INPUT_FILE="$1" duckdb-cli
+
+    if [ ! -f $1 ]; then
+        echo "File $1 does not exist"
+        exit 1
+    else
+        cp $1 $std_input_file
+    fi
+
 else
-    docker compose -f docker-compose-duckdb.yml run --rm duckdb-cli
+    echo "A file must be informed as a parameter"
+    exit 1
 fi
+
+echo "Executing duckdb script $1"
+docker compose -f $project_root/src/main/docker/duckdb/docker-compose-duckdb.yml run --rm duckdb-cli
+rm $std_input_file
+
+exit 0
