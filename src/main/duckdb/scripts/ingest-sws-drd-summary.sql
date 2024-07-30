db1dfab0b2eb0a36a58f16ecc3043f353a4c7e87
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-07-30 22:08:49 +0200

    flyway fixes & duckdb PRQL POC

--- src/main/duckdb/scripts/ingest-sws-drd-summary.sql
+++ src/main/duckdb/scripts/ingest-sws-drd-summary.sql
@@ -1,11 +1,29 @@
 CREATE TEMP TABLE sws_summary AS
-    SELECT *
+    SELECT asset, current_value
     FROM read_csv(
-        '/duckdb/input/us-complete-portfolio-summary.csv'--,
---         columns = {
---             'asset': 'text',
---             'total_bought': ''
---         }
-    );
-    
-SELECT * FROM sws_summary;
+        '/duckdb/input/us-complete-portfolio-summary.csv',
+        columns = {
+            'asset': 'TEXT',
+            'total_bought': 'NUMERIC',
+            'total_shares': 'SMALLINT',
+            'current_price': 'NUMERIC',
+            'current_value': 'NUMERIC',
+            'capital_gains': 'NUMERIC',
+            'dividends': 'NUMERIC',
+            'total_gain_currency': 'NUMERIC',
+            'average_years': 'SMALLINT',
+            'total_return': 'TEXT'
+        }
+    )
+    WHERE current_value > 0;
+
+install prql FROM community;
+load prql;
+
+(|
+    from sws_summary
+    select {
+        asset_ticker = asset,
+        total_market_value = current_value
+    }
+|);
