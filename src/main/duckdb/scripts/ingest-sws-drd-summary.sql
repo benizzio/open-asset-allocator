3a34b3f47f403bb1dffd2e0add580c3a4d02c007
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-07-30 22:32:07 +0200

    POC of data ingestion from duckdb to postgres

--- src/main/duckdb/scripts/ingest-sws-drd-summary.sql
+++ src/main/duckdb/scripts/ingest-sws-drd-summary.sql
@@ -15,15 +15,32 @@ CREATE TEMP TABLE sws_summary AS
             'total_return': 'TEXT'
         }
     )
-    WHERE current_value > 0;
+    WHERE current_value > 0
+;
 
 install prql FROM community;
+install postgres;
 load prql;
+load postgres;
 
-(|
-    from sws_summary
-    select {
-        asset_ticker = asset,
-        total_market_value = current_value
-    }
-|);
+ATTACH 'dbname=postgres user=postgres password=local host=172.17.0.1' AS postgres (TYPE POSTGRES);
+
+BEGIN TRANSACTION;
+
+INSERT INTO postgres.asset (ticker)
+    SELECT swss.asset AS ticker FROM sws_summary swss
+    LEFT JOIN postgres.asset ass ON swss.asset = ass.ticker
+    WHERE ass.ticker IS NULL
+;
+
+-- (|
+--     from sws_summary
+--     select {
+--         asset_ticker = asset,
+--         total_market_value = current_value
+--     }
+-- |);
+
+COMMIT;
+
+SELECT * FROM postgres.asset;
