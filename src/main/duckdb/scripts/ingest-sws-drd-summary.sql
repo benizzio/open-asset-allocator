584e30fb4706d7f4d13fe126258773bd80a5fb2b
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-08-02 00:34:26 +0200

    progressed on data ingestion for sws and fixed transaction in duckdb cli script

--- src/main/duckdb/scripts/ingest-sws-drd-summary.sql
+++ src/main/duckdb/scripts/ingest-sws-drd-summary.sql
@@ -1,5 +1,9 @@
+.bail on
+.changes on
+
+.print '=> Reading the Simply Wall Street Summary file'
 CREATE TEMP TABLE sws_summary AS
-    SELECT asset, current_value
+    SELECT asset, total_shares, current_price, current_value
     FROM read_csv(
         '/duckdb/input/us-complete-portfolio-summary.csv',
         columns = {
@@ -18,6 +22,21 @@ CREATE TEMP TABLE sws_summary AS
     WHERE current_value > 0
 ;
 
+.print '=> Reading the asset dimension mapping classifier file'
+CREATE TEMP TABLE asset_dimension_mapping AS
+    SELECT *
+    FROM read_csv(
+        '/duckdb/input/asset-dimension-mapping.csv',
+        columns = {
+            'ticker': 'TEXT',
+            'class': 'TEXT',
+            'asset_quantity': 'NUMERIC',
+            'cash_reserve': 'BOOLEAN'
+        }
+    )
+;
+
+.print '=> Loading external modules and databases needed for ingestion'
 install prql from community;
 install postgres;
 load prql;
@@ -25,12 +44,32 @@ load postgres;
 
 ATTACH '' AS pgsql (TYPE POSTGRES);
 
+.print '=> Starting data ingestion transaction'
 BEGIN TRANSACTION;
 
+.print '=> Inserting asset data into the asset table the WHEN it does not exist'
 INSERT INTO pgsql.asset (ticker)
     SELECT swss.asset AS ticker FROM sws_summary swss
     LEFT JOIN pgsql.asset ass ON swss.asset = ass.ticker
     WHERE ass.ticker IS NULL
+;
+
+.print '=> Inserting portfolio data in the asset fact table, joining data classification from asset dimension mapping classifier file'
+-- Has to fail when the asset dimension data is missing to indicate that file is broken and needs more data
+-- INSERT INTO pgsql.asset_value_fact
+    -- (asset_id, class, cash_reserve, asset_quantity, asset_market_price, total_market_value)
+
+    -- TODO fix this query
+    SELECT
+        ass.id,
+        adm.class,
+        adm.cash_reserve,
+        nullif(adm.asset_quantity, -1),
+        constant_or_null(swss.current_price, adm.asset_quantity),
+        swss.current_value
+    FROM sws_summary swss
+    LEFT JOIN asset_dimension_mapping adm ON adm.ticker = swss.asset
+    LEFT JOIN pgsql.asset ass ON ass.ticker = swss.asset
 ;
 
 -- (|
