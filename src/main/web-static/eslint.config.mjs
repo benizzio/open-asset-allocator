44a7c56275c6f7d1adc502670359c1ddd8b66360
Igor Benicio de Mesquita <benizzio@gmail.com>
2024-09-29 22:13:47 +0200

    creating POC for backend/frontend stack (WIP)
    added and configured linter/formatter for Typescript

---
+++ src/main/web-static/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import stylisticTs from "@stylistic/eslint-plugin-ts";
+
+export default [
+
+    { files: ["**/*.{js,mjs,cjs,ts}"] },
+
+    { languageOptions: { globals: globals.browser } },
+
+    pluginJs.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        plugins: { "@stylistic/ts": stylisticTs },
+        rules: {
+            "max-len": ["warn", { code: 120, tabWidth: 4 }],
+            "@stylistic/ts/indent": ["warn", 4],
+            "@stylistic/ts/semi": ["error", "always"],
+            "@stylistic/ts/quotes": ["warn", "double"],
+            "@stylistic/ts/quote-props": ["warn", "consistent-as-needed"],
+            "@stylistic/ts/object-curly-newline": ["warn", { multiline: true }],
+            "@stylistic/ts/object-curly-spacing": ["warn", "always"],
+            "@stylistic/ts/comma-dangle": ["warn", "always-multiline"],
+            "@stylistic/ts/comma-spacing": ["warn", { before: false, after: true }],
+            "@stylistic/ts/key-spacing": ["warn", { beforeColon: false, afterColon: true }],
+            "@stylistic/ts/padding-line-between-statements": [
+                "warn",
+                { blankLine: "always", prev: "import", next: "*" },
+                { blankLine: "never", prev: "import", next: "import" },
+                { blankLine: "always", prev: "*", next: "multiline-expression" },
+                { blankLine: "always", prev: "*", next: "multiline-block-like" },
+                { blankLine: "always", prev: "*", next: "multiline-const" },
+                { blankLine: "always", prev: "*", next: "multiline-let" },
+                { blankLine: "always", prev: "*", next: "multiline-var" }],
+        },
+    },
+];
--- src/main/web-static/script/root.ts
+++ src/main/web-static/script/root.ts
@@ -1,19 +1,11 @@
-import {Chart, registerables} from 'chart.js'
+import { Chart, registerables } from "chart.js";
 
 Chart.register(...registerables);
 
-const chartCanvas = document.getElementById('testChart') as HTMLCanvasElement;
+const chartCanvas = document.getElementById("testChart") as HTMLCanvasElement;
 
-const chart = new Chart(chartCanvas, {
-    type: 'pie',
-    data: {
-        datasets: [{
-            data: [10, 20, 30],
-            label: 'Test dataset'
-        }],
-    },
-    options: {
-        responsive: true,
-        maintainAspectRatio: true,
-    }
+new Chart(chartCanvas, {
+    type: "pie",
+    data: { datasets: [{ data: [10, 20, 30], label: "Test dataset" }] },
+    options: { responsive: true, maintainAspectRatio: true },  
 });
