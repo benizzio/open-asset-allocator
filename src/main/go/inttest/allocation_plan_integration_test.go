8690992546ab95f28b8ad04093a4ddaef6f04055
Igor Benicio de Mesquita <benizzio@gmail.com>
2025-04-21 10:38:33 +0200

    completing POC test implementation

--- src/main/go/inttest/allocation_plan_integration_test.go
+++ src/main/go/inttest/allocation_plan_integration_test.go
@@ -1,14 +1,13 @@
 package inttest
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"io"
 	"net/http"
 	"testing"
 )
 
-func TestGetAllocationPlans(t *testing.T) { //TODO complete infra and test
+func TestGetAllocationPlans(t *testing.T) {
 
 	response, err := http.Get("http://localhost:8081/api/portfolio/1/allocation-plan")
 	assert.NoError(t, err)
@@ -20,5 +19,62 @@ func TestGetAllocationPlans(t *testing.T) { //TODO complete infra and test
 	assert.NotEmpty(t, body)
 
 	// TODO continue with assertions
-	fmt.Println(string(body))
+	var expectedResponseJSON = `
+		[
+			{
+				"id":1,
+				"name":"60/40 Portfolio Classic - Example",
+				"type":"ALLOCATION_PLAN",
+				"details":[
+					{
+						"structuralId":["NasdaqGM:SHV","STOCKS"],
+						"cashReserve":true,
+						"sliceSizePercentage":"0.5"
+					},
+					{
+						"structuralId":[null,"BONDS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.6"
+					},
+					{
+						"structuralId":["ARCA:SPY","STOCKS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.45"
+					},
+					{
+						"structuralId":[null,"STOCKS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.4"
+					},
+					{
+						"structuralId":["ARCA:BIL","BONDS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.4"
+					},
+					{
+						"structuralId":["NasdaqGM:IEF","BONDS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.3"
+					},
+					{
+						"structuralId":["NasdaqGM:TLT","BONDS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.2"
+					},
+					{
+						"structuralId":["ARCA:STIP","BONDS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.1"
+					},
+					{
+						"structuralId":["ARCA:EWZ","STOCKS"],
+						"cashReserve":false,
+						"sliceSizePercentage":"0.05"
+					}
+				]
+			}
+		]
+	`
+
+	assert.JSONEq(t, expectedResponseJSON, string(body))
 }
