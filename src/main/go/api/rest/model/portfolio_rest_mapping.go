ccbc5053d0e9b3fa9854e6c5acda587725d1726a
Igor Benicio de Mesquita <benizzio@gmail.com>
2025-06-22 12:29:33 +0200

    addressing review comments

--- src/main/go/api/rest/model/portfolio_rest_mapping.go
+++ src/main/go/api/rest/model/portfolio_rest_mapping.go
@@ -140,7 +140,7 @@ func buildSnapshotDTS(
 	}
 }
 
-func mapToObservationTimestampsDTS(
+func mapToObservationTimestampDTSs(
 	availableObservationTimestamps []*domain.PortfolioObservationTimestamp,
 ) []*PortfolioObservationTimestampDTS {
 	var observationTimestampsDTS = make([]*PortfolioObservationTimestampDTS, 0)
@@ -166,7 +166,7 @@ func mapToObservationTimestampDTS(observationTimestamp *domain.PortfolioObservat
 
 func MapToAnalysisOptionsDTS(analysisOptions *domain.AnalysisOptions) *AnalysisOptionsDTS {
 	var plans = mapToAllocationPlanIdentifierDTSs(analysisOptions.AvailablePlans)
-	var availableHistory = mapToObservationTimestampsDTS(analysisOptions.AvailableObservationTimestamps)
+	var availableHistory = mapToObservationTimestampDTSs(analysisOptions.AvailableObservationTimestamps)
 	return &AnalysisOptionsDTS{
 		AvailableHistory:         analysisOptions.AvailableHistory,
 		AvailableObservedHistory: availableHistory,
