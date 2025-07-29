074add8916cc04d3297fc7a1822fda4fc24c8730
Igor Benicio de Mesquita <benizzio@gmail.com>
2025-07-29 20:59:16 +0200

    portfolio allocation history management (WIP)
    created utility to run db cleanup function with multiple statements

--- src/main/go/inttest/util/manage_db.go
+++ src/main/go/inttest/util/manage_db.go
@@ -6,12 +6,47 @@ import (
 	"github.com/golang/glog"
 )
 
+type testFormattableSQLPair struct {
+	formattableSQL string
+	params         []any
+}
+
+type CleanupFunctionBuilder struct {
+	cleanupQueries []*testFormattableSQLPair
+}
+
+func (builder *CleanupFunctionBuilder) AddCleanupQuery(formattableSQL string, params ...any) *CleanupFunctionBuilder {
+	builder.cleanupQueries = append(
+		builder.cleanupQueries, &testFormattableSQLPair{
+			formattableSQL: formattableSQL,
+			params:         params,
+		},
+	)
+	return builder
+}
+
+func (builder *CleanupFunctionBuilder) Build() func() {
+	return createDBCleanupFunctionMulti(builder.cleanupQueries)
+}
+
+func BuildTestCleanupSQLBuilder() *CleanupFunctionBuilder {
+	return &CleanupFunctionBuilder{
+		cleanupQueries: make([]*testFormattableSQLPair, 0),
+	}
+}
+
 func CreateDBCleanupFunction(formattableSQL string, params ...any) func() {
+	return createDBCleanupFunctionMulti([]*testFormattableSQLPair{{formattableSQL, params}})
+}
+
+func createDBCleanupFunctionMulti(cleanupQueries []*testFormattableSQLPair) func() {
 	return func() {
-		glog.Infof("Executing test cleanup query: %s", formattableSQL)
-		err := inttestinfra.ExecuteDBQuery(fmt.Sprintf(formattableSQL, params...))
-		if err != nil {
-			glog.Errorf("Error executing cleanup query: %s", err)
+		for _, query := range cleanupQueries {
+			glog.Infof("Executing test cleanup query: %s", query.formattableSQL)
+			err := inttestinfra.ExecuteDBQuery(fmt.Sprintf(query.formattableSQL, query.params...))
+			if err != nil {
+				glog.Errorf("Error executing cleanup query: %s", err)
+			}
 		}
 	}
 }
