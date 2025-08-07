ae8481971298c06a667079c0cec84409ba969388
Igor Benicio de Mesquita <benizzio@gmail.com>
2025-08-07 23:37:02 +0200

    Time frame tag substitution - DB & ingestion
    fixed file formatting

--- src/main/flyway/sql/migration-[11]-portfolio_allocation_fact_time_frame_tag_substitution.sql
+++ src/main/flyway/sql/migration-[11]-portfolio_allocation_fact_time_frame_tag_substitution.sql
@@ -1,6 +1,7 @@
-ALTER TABLE portfolio_allocation_fact DROP CONSTRAINT portfolio_allocation_fact_pk ;
+ALTER TABLE portfolio_allocation_fact DROP CONSTRAINT portfolio_allocation_fact_pk;
 
 ALTER TABLE portfolio_allocation_fact
-    ADD CONSTRAINT portfolio_allocation_fact_pk PRIMARY KEY (portfolio_id, class, cash_reserve, asset_id, observation_time_id);
+    ADD CONSTRAINT portfolio_allocation_fact_pk PRIMARY KEY (portfolio_id, class, cash_reserve, asset_id, observation_time_id)
+;
 
 ALTER TABLE portfolio_allocation_fact DROP COLUMN time_frame_tag;
