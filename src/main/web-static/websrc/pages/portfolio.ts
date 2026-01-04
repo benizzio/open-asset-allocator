c377be3317bb87e656c22bfff82b5be968c723c2
Igor Benicio de Mesquita <benizzio@gmail.com>
2026-01-04 14:44:07 +0100

    addressing review comments

--- src/main/web-static/websrc/pages/portfolio.ts
+++ src/main/web-static/websrc/pages/portfolio.ts
@@ -6,8 +6,8 @@ const PortfolioPage = {
     getContextPortfolio(): Portfolio {
         const portfolioElement = window["portfolio"] as HTMLScriptElement;
         const portfolioJSON = portfolioElement.textContent;
-        const porfolioDTO = JSON.parse(portfolioJSON) as PortfolioDTO;
-        return DomainService.mapping.mapToPortfolio(porfolioDTO);
+        const portfolioDTO = JSON.parse(portfolioJSON) as PortfolioDTO;
+        return DomainService.mapping.mapToPortfolio(portfolioDTO);
     },
 };
 
