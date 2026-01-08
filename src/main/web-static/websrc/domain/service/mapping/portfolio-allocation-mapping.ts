d228ddf3025eb3ee50410f06c8556a57e80dbad5
Igor Benicio de Mesquita <benizzio@gmail.com>
2026-01-08 20:04:21 +0100

    addressing review comment

--- src/main/web-static/websrc/domain/service/mapping/portfolio-allocation-mapping.ts
+++ src/main/web-static/websrc/domain/service/mapping/portfolio-allocation-mapping.ts
@@ -4,11 +4,22 @@ import { PortfolioSnapshot, PortfolioSnapshotDTO } from "../../portfolio-allocat
 export function mapToPortfolioSnapshot(
     dto: PortfolioSnapshotDTO,
 ): PortfolioSnapshot {
+
     return {
+
         ...dto,
-        allocations: dto.allocations.map((allocationDTO) => ({
-            ...allocationDTO,
-            totalMarketValue: new BigNumber(allocationDTO.totalMarketValue),
-        })),
+        allocations: dto.allocations.map((allocationDTO) => {
+
+            const totalMarketValue = new BigNumber(allocationDTO.totalMarketValue);
+
+            if(totalMarketValue.isNaN()) {
+                throw new Error(`Invalid totalMarketValue value: ${ allocationDTO.totalMarketValue }`);
+            }
+
+            return {
+                ...allocationDTO,
+                totalMarketValue: totalMarketValue,
+            };
+        }),
     };
 }
