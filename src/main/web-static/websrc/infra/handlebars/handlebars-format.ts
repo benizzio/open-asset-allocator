228ff20cbfc52d14be5bcc8f22d9e189d12ca834
Igor Benicio de Mesquita <benizzio@gmail.com>
2026-03-01 19:45:43 +0100

    addressing review comments

--- src/main/web-static/websrc/infra/handlebars/handlebars-format.ts
+++ src/main/web-static/websrc/infra/handlebars/handlebars-format.ts
@@ -4,7 +4,7 @@ import Format from "../format";
 const handlebarsFormatCurrency = (value: number | string, currency: unknown) => {
     const localCurrency = typeof currency === "string" ? currency : undefined;
     const numericValue = typeof value === "number" ? value : parseFloat(value);
-    return Format.formatCurrency(numericValue, localCurrency);
+    return Format.formatCurrency(numericValue, undefined, localCurrency);
 };
 
 export function registerHandlebarsFormatHelper() {
