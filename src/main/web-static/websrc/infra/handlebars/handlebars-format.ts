0e82d8188370b420e7ee3e46a6b7fe14e432b30c
Igor Benicio de Mesquita <benizzio@gmail.com>
2026-03-01 19:33:26 +0100

    fixing handlebars currency format helper

--- src/main/web-static/websrc/infra/handlebars/handlebars-format.ts
+++ src/main/web-static/websrc/infra/handlebars/handlebars-format.ts
@@ -1,9 +1,10 @@
 import * as handlebars from "handlebars";
 import Format from "../format";
 
-const hanldebarsFormatCurrency = (value: number, currency: unknown) => {
+const hanldebarsFormatCurrency = (value: number | string, currency: unknown) => {
     const localCurrency = typeof currency === "string" ? currency : undefined;
-    return Format.formatCurrency(value, localCurrency);
+    const numericValue = typeof value === "number" ? value : parseFloat(value);
+    return Format.formatCurrency(numericValue, localCurrency);
 };
 
 export function registerHandlebarsFormatHelper() {
