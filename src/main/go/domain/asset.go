ce32d02ba4b242f59b66b76359545c7e8c9d806e
Igor Benicio de Mesquita <benizzio@gmail.com>
2026-03-28 22:58:11 +0100

    adding model properties for external asset data

--- src/main/go/domain/asset.go
+++ src/main/go/domain/asset.go
@@ -2,12 +2,39 @@ package domain
 
 import (
 	"context"
+	"fmt"
+
+	"github.com/benizzio/open-asset-allocator/infra"
 )
 
 type Asset struct {
 	Id     int64
 	Name   string
 	Ticker string
+}
+
+// TODO json marshalling for DB (Scan and Value)
+type ExternalData struct {
+	Data []ExternalAssetData `json:"data"`
+}
+
+type ExternalAssetData struct {
+	Source AssetExternalSource `json:"source"`
+	Ticker string              `json:"ticker"`
+}
+
+type AssetExternalSource string
+
+const (
+	YahooFinanceSource AssetExternalSource = "YAHOO_FINANCE"
+)
+
+func (externalSource AssetExternalSource) Validate() error {
+	switch externalSource {
+	case YahooFinanceSource:
+		return nil
+	}
+	return infra.BuildDomainValidationError(fmt.Sprintf("Invalid AssetExternalSource %s", externalSource), nil)
 }
 
 type AssetsPerTicker map[string]*Asset
